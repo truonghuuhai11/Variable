0198a4aa39431baedab587d2ff8e6331fc363cba
Trương Hữu Hải <144077948+truonghuuhai11@users.noreply.github.com>
2023-12-16 07:40:35 +0700

    Add files via upload

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,27 @@
+package basic.dev;
+
+/**
+ *
+ * @author TranThanhBinh
+ */
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/**
+		 * Khai baìo biêìn
+		 */
+		String firstName = "Phan Van";
+		String lastName = "Nam";
+		int old = 15;
+		float markMath = 9f;
+		boolean passed = true;
+		System.out.println("Ho va ten:");
+		System.out.println(firstName + " " + lastName);
+		System.out.println("Mark Math: " + markMath);
+		System.out.println("Passed: " + passed);
+//Khai baìo hãÌng. HãÌng phaÒi viêìt hoa
+		final float PI = 3.14f;
+	}
+
+}
